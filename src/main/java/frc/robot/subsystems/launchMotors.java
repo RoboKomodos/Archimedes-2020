cc5b9cf0bdb7af262cff81fc4a1a4ec9b8324be8
Connor Gannaway <ccgannaway@me.com>
2020-11-04 15:37:29 -0600

    Launchmotors (#4)
    * Ryan/Jackson - wrote launchmotors code. still needs ports/constants
    
    * renamed and updated launch motors and constants

---
+++ src/main/java/frc/robot/subsystems/launchMotors.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class launchMotors extends SubsystemBase {
+  /**
+   * Creates a new launchMotors.
+   */
+  Victor motor1;
+  Victor motor2;
+  public launchMotors() 
+  {
+    motor1 = new Victor(Constants.launchMotor1);
+    motor2 = new Victor(Constants.launchMotor2);
+  }
+  public void startLaunchMotor()
+  {
+    motor1.setSpeed(Constants.launchMotorSpeed);
+    motor2.setSpeed(Constants.launchMotorSpeed);
+  }
+
+  public void stopLaunchMotor()
+  {
+    motor1.setSpeed(0.0);
+    motor2.setSpeed(0.0);
+  }
+  /**
+   * Toggles motors on or off based on if they are already spinning
+   */
+  public void areTheMotorsOn()
+  {
+    double checkMotor1 = motor1.getSpeed();
+    if(checkMotor1==0.0)
+    {
+      startLaunchMotor();
+    }
+    else
+    {
+      stopLaunchMotor();
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
